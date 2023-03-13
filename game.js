8929594a0288900de524d901aaef22862c53c514
carlmarkegang <huledule@hotmail.com>
2023-03-13 18:58:13 +0100

    Some more gameplay

--- game.js
+++ game.js
@@ -10,13 +10,15 @@ var Engine = Matter.Engine,
 Events = Matter.Events;
 // create engine
 var engine = Engine.create(), world = engine.world;
+engine.gravity.y = 0.4;
+
 // create renderer
 var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
         width: 1000,
-        height: 500,
+        height: 650,
         wireframes: false,
         background: '#edebd1'
     }
@@ -39,18 +41,39 @@ var rotated_block_1 = Bodies.rectangle(200, 400, 200, 50, {
         lineWidth: 0
     }
 });
+var rotated_block_2 = Bodies.rectangle(700, 450, 400, 200, {
+    isStatic: true,
+    render: {
+        fillStyle: '#1e6b28',
+        lineWidth: 0
+    }
+});
+
+var rotated_block_3 = Bodies.rectangle(2000, 0, 200, 1000, {
+    isStatic: true,
+    render: {
+        fillStyle: '#1e6b28',
+        lineWidth: 0
+    }
+});
 
 
 World.add(world, [
     car,
     ground,
-    rotated_block_1
+    rotated_block_1,
+    rotated_block_2,
+    rotated_block_3
 ]);
 
 
 
 Body.rotate(rotated_block_1, 0.7);
 Matter.Body.set(rotated_block_1, "position", { x: 300, y: 430 })
+
+Body.rotate(rotated_block_2, 0.3);
+
+Body.rotate(rotated_block_3, 0.4);
 
 function create_car(xx, yy, width, height, wheelSize) {
     var group = Body.nextGroup(true),
@@ -80,7 +103,10 @@ function create_car(xx, yy, width, height, wheelSize) {
         },
         render: {
             fillStyle: '#3f4142',
-            lineWidth: 0
+            lineWidth: 0,
+            sprite: {
+                texture: './assets/wheel.png'
+            }
         },
         friction: 1
     });
@@ -91,7 +117,10 @@ function create_car(xx, yy, width, height, wheelSize) {
         },
         render: {
             fillStyle: '#3f4142',
-            lineWidth: 0
+            lineWidth: 0,
+            sprite: {
+                texture: './assets/wheel.png'
+            }
         },
         friction: 1
     });
@@ -112,9 +141,10 @@ function create_car(xx, yy, width, height, wheelSize) {
         length: 0
     });
 
-    Composite.addBody(car, body);
+    
     Composite.addBody(car, wheelA);
     Composite.addBody(car, wheelB);
+    Composite.addBody(car, body);
     Composite.addConstraint(car, axelA);
     Composite.addConstraint(car, axelB);
 
@@ -126,34 +156,40 @@ function create_car(xx, yy, width, height, wheelSize) {
 
 
 var keysDown = {};
-var WheelSpeed = 0.5;
+var WheelSpeed = 0.2;
 var CameraFollow = 0;
 function update() {
+    if (16 in keysDown ) {
+        WheelSpeed = 0.4;
+    } else {
+        WheelSpeed = 0.2;
+    }
 
     if (68 in keysDown || 39 in keysDown) {
-        // Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: 0.008, y: 0 });
+        Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: 0.001, y: 0 });
+        Body.setAngularVelocity(car.bodies[0], WheelSpeed)
         Body.setAngularVelocity(car.bodies[1], WheelSpeed)
-        Body.setAngularVelocity(car.bodies[2], WheelSpeed)
         if (CameraFollow > -100) {
             CameraFollow -= 1;
         }
 
     }
     if (65 in keysDown || 37 in keysDown) {
-        //Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: -0.008, y: 0 });
+        Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: -0.001, y: 0 });
+        Body.setAngularVelocity(car.bodies[0], -WheelSpeed)
         Body.setAngularVelocity(car.bodies[1], -WheelSpeed)
-        Body.setAngularVelocity(car.bodies[2], -WheelSpeed)
         if (CameraFollow > 100) {
             CameraFollow += 1;
         }
     }
 
     if (87 in keysDown || 38 in keysDown) {
-        Body.setAngularVelocity(car.bodies[0], 0.05)
+        //Body.setAngularVelocity(car.bodies[2], 0.05)
+        Body.applyForce(car.bodies[2], { x: car.bodies[2].position.x, y: car.bodies[2].position.y }, { x: -0, y: -0.005 });
     }
 
     if (83 in keysDown || 40 in keysDown) {
-        Body.setAngularVelocity(car.bodies[0], -0.05)
+        Body.setAngularVelocity(car.bodies[2], -0.05)
     }
 
 
@@ -168,6 +204,6 @@ Events.on(render, 'beforeRender', function () {
     Render.lookAt(render, car.bodies[0], {
         x: 300 + CameraFollow,
         y: 400
-    }, false);
+    }, true);
 });
 let drawInterval = setInterval(update, 7);
