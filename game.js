986a0628ab6742c1cf7fd22aa7a9bbf5360bca0f
Carl Markegång <huledule@hotmail.com>
2023-03-13 12:44:42 +0100

    making the wheels spin instead of applying force

--- game.js
+++ game.js
@@ -24,7 +24,7 @@ var render = Render.create({
 Engine.run(engine);
 Render.run(render);
 
-var car = create_car(150, 100, 150, 30, 30);
+var car = create_car(150, 100, 150, 30, 40);
 var ground = Bodies.rectangle(400, 400, 8100, 30, {
     isStatic: true,
     render: {
@@ -82,7 +82,7 @@ function create_car(xx, yy, width, height, wheelSize) {
             fillStyle: '#3f4142',
             lineWidth: 0
         },
-        friction: 0.8
+        friction: 1
     });
 
     var wheelB = Bodies.circle(xx + wheelBOffset, yy + wheelYOffset, wheelSize, {
@@ -93,7 +93,7 @@ function create_car(xx, yy, width, height, wheelSize) {
             fillStyle: '#3f4142',
             lineWidth: 0
         },
-        friction: 0.8
+        friction: 1
     });
 
     var axelA = Constraint.create({
@@ -126,12 +126,26 @@ function create_car(xx, yy, width, height, wheelSize) {
 
 
 var keysDown = {};
+var WheelSpeed = 0.5;
+var CameraFollow = 0;
 function update() {
+
     if (68 in keysDown || 39 in keysDown) {
-        Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: 0.008, y: 0 });
+        // Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: 0.008, y: 0 });
+        Body.setAngularVelocity(car.bodies[1], WheelSpeed)
+        Body.setAngularVelocity(car.bodies[2], WheelSpeed)
+        if (CameraFollow > -100) {
+            CameraFollow -= 1;
+        }
+
     }
     if (65 in keysDown || 37 in keysDown) {
-        Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: -0.008, y: 0 });
+        //Body.applyForce(car.bodies[0], { x: car.bodies[0].position.x, y: car.bodies[0].position.y }, { x: -0.008, y: 0 });
+        Body.setAngularVelocity(car.bodies[1], -WheelSpeed)
+        Body.setAngularVelocity(car.bodies[2], -WheelSpeed)
+        if (CameraFollow > 100) {
+            CameraFollow += 1;
+        }
     }
 
     if (87 in keysDown || 38 in keysDown) {
@@ -141,6 +155,8 @@ function update() {
     if (83 in keysDown || 40 in keysDown) {
         Body.setAngularVelocity(car.bodies[0], -0.05)
     }
+
+
 }
 window.addEventListener('keydown', function (e) {
     keysDown[e.keyCode] = true;
@@ -150,8 +166,8 @@ window.addEventListener('keyup', function (e) {
 });
 Events.on(render, 'beforeRender', function () {
     Render.lookAt(render, car.bodies[0], {
-        x: 400,
+        x: 300 + CameraFollow,
         y: 400
-    });
+    }, false);
 });
 let drawInterval = setInterval(update, 7);
