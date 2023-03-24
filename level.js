fa1c88cda47d7c7b00fc86522143efb1739166e3
carlmarkegang <huledule@hotmail.com>
2023-03-24 22:58:39 +0100

    more level building

--- level.js
+++ level.js
@@ -1,7 +1,24 @@
 
+
 staticBlocks.push(new create_staticBlock(200,400,200,50,0));
 staticBlocks.push(new create_staticBlock(-284,386,200,50,0.43500000000000033));
 staticBlocks.push(new create_staticBlock(538,506,500,500,0));
+staticBlocks.push(new create_staticBlock(-462,324,200,50,6.521));
+staticBlocks.push(new create_staticBlock(-634,254,200,50,6.8199999999999985));
+staticBlocks.push(new create_staticBlock(-780,156,200,50,0.6539999999999959));
+staticBlocks.push(new create_staticBlock(-884,26,200,50,-1.941000000000007));
+staticBlocks.push(new create_staticBlock(-962,-154,200,50,-1.941000000000007));
+staticBlocks.push(new create_staticBlock(142,356,200,50,2.415000000000001));
+staticBlocks.push(new create_staticBlock(300,248,200,50,8.963000000000003));
+staticBlocks.push(new create_staticBlock(468,230,200,50,15.948000000000004));
+staticBlocks.push(new create_staticBlock(-926,-272,200,50,-0.6660000000000005));
+staticBlocks.push(new create_staticBlock(-780,-342,200,50,-3.252));
+staticBlocks.push(new create_staticBlock(-960,-298,200,50,-3.542));
+staticBlocks.push(new create_staticBlock(-588,552,500,500,0.38900000000000035));
+staticBlocks.push(new create_staticBlock(-938,370,500,500,0.6570000000000006));
+staticBlocks.push(new create_staticBlock(-1134,38,500,500,2.786));
+staticBlocks.push(new create_staticBlock(-1430,298,1000,1000,-0.1900000000000001));
+
 
 World.add(world, [
     car,
